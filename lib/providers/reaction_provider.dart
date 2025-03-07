0c867535e5c7f6dd4a8f047c5907ce2d3824a70d
Владимир <malyshev-vd@mail.ru>
2025-03-07 17:39:15 +0300

    feat: изменен главный экран

--- lib/providers/reaction_provider.dart
+++ lib/providers/reaction_provider.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:reaction_speed_trainer/repositories/i_reaction_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum SortOrder { ascending, descending }
 
 class ReactionProvider with ChangeNotifier {
-  List<Map<String, dynamic>> _results = [];
+  // Отдельные списки для хранения данных разных типов
+  List<Map<String, dynamic>> _measurementResults = [];
+  List<Map<String, dynamic>> _levelResults = [];
+
   SortOrder _sortOrder = SortOrder.descending;
 
-  final DatabaseReference _database = FirebaseDatabase.instance.ref();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final IReactionRepository _repository;
 
-  User? get currentUser => _auth.currentUser;
-  String get userId => currentUser?.uid ?? 'default_user_id';
+  ReactionProvider(this._repository);
 
   SortOrder get sortOrder => _sortOrder;
 
-  ReactionProvider() {
-    _auth.authStateChanges().listen((user) {
-      if (user != null) {
-        loadResults();
-      } else {
-        _results.clear();
-        notifyListeners();
-      }
-    });
-  }
+  // Геттеры для получения отсортированных данных
+  List<Map<String, dynamic>> get measurementResults => _getSortedResults(_measurementResults);
+  List<Map<String, dynamic>> get levelResults => _getSortedResults(_levelResults);
 
-  void setSortOrder(SortOrder order) {
-    _sortOrder = order;
-    notifyListeners();
-  }
-
-  List<Map<String, dynamic>> get results {
-    final sortedResults = List<Map<String, dynamic>>.from(_results);
+  // Общая логика сортировки
+  List<Map<String, dynamic>> _getSortedResults(List<Map<String, dynamic>> results) {
+    final sortedResults = List<Map<String, dynamic>>.from(results);
     if (_sortOrder == SortOrder.descending) {
       sortedResults.sort((a, b) => DateTime.parse(b['date']).compareTo(DateTime.parse(a['date'])));
     } else {
@@ -42,92 +32,55 @@ class ReactionProvider with ChangeNotifier {
     return sortedResults;
   }
 
-  
-
- Future<void> loadResults({
-  String type = 'measurements',
-  String? levelId,
-  String? difficulty,
-}) async {
-  if (_auth.currentUser == null) {
-    _results = [];
+  void setSortOrder(SortOrder order) {
+    _sortOrder = order;
     notifyListeners();
-    return;
   }
 
-  try {
-    DatabaseReference ref;
+  Future<void> loadResults({
+    required String type,
+    String? levelId,
+    String? difficulty,
+  }) async {
+    try {
+      final results = await _repository.loadResults(
+        type: type,
+        levelId: levelId,
+        difficulty: difficulty,
+      );
 
-    if (type == 'measurements') {
-      ref = _database.child('users/$userId/results/measurements');
-    } else if (type == 'levels' && levelId != null) {
-      if (difficulty != null) {
-        ref = _database.child('users/$userId/results/levels/$levelId/$difficulty');
-      } else {
-        ref = _database.child('users/$userId/results/levels/$levelId');
+      if (type == 'measurements') {
+        _measurementResults = results;
+      } else if (type == 'levels') {
+        _levelResults = results;
       }
-    } else {
-      throw Exception('Некорректный тип результата');
-    }
 
-    final snapshot = await ref.orderByChild('date').get();
-
-    if (snapshot.exists) {
-      _results = snapshot.children.map((doc) {
-        if (doc.value is Map) {
-          final data = Map<String, dynamic>.from(doc.value as Map);
-          data['id'] = doc.key;
-          return data;
-        } else {
-          throw Exception('Некорректный формат данных');
-        }
-      }).toList();
-    } else {
-      _results = [];
+      notifyListeners();
+    } catch (e) {
+      print('Ошибка загрузки данных: $e');
     }
-  } catch (e) {
-    print('Ошибка загрузки данных: $e');
-    _results = [];
-  } finally {
-    notifyListeners();
   }
-}
 
   Future<void> addResult({
     required String type,
     required double time,
+    int? repetitions,
+    int? errors,
     String? levelId,
     String? difficulty,
+    String? date,
   }) async {
-    if (currentUser == null) {
-      throw Exception('Пользователь не авторизован');
-    }
-
-    final Map<String, dynamic> newResult = {
-      'userId': userId,
-      'time': time,
-      'date': DateTime.now().toString(),
-    };
-
     try {
-      DatabaseReference resultRef;
-
-      if (type == 'measurements') {
-        resultRef = _database.child('users/$userId/results/measurements').push();
-      } else if (type == 'levels' && levelId != null && difficulty != null) {
-        resultRef = _database
-            .child('users/$userId/results/levels/$levelId/$difficulty')
-            .push();
-      } else {
-        throw Exception('Некорректный тип результата');
-      }
-
-      await resultRef.set(newResult);
-      print('Данные успешно записаны в базу: $newResult');
-
-      newResult['id'] = resultRef.key;
-      _results.add(newResult);
-      notifyListeners();
+      await _repository.addResult(
+        type: type,
+        time: time,
+        repetitions: repetitions,
+        errors: errors,
+        levelId: levelId,
+        difficulty: difficulty,
+        date: date
+      );
+      await loadResults(type: type, levelId: levelId, difficulty: difficulty,);
     } catch (e) {
       print('Ошибка при добавлении результата: $e');
     }
@@ -139,23 +92,20 @@ class ReactionProvider with ChangeNotifier {
     String? levelId,
     String? difficulty,
   }) async {
-    if (currentUser == null) {
-      throw Exception('Пользователь не авторизован');
-    }
-
     try {
-      DatabaseReference ref;
+      await _repository.deleteResultById(
+        id: id,
+        type: type,
+        levelId: levelId,
+        difficulty: difficulty,
+      );
 
       if (type == 'measurements') {
-        ref = _database.child('users/$userId/results/measurements/$id');
-      } else if (type == 'levels' && levelId != null && difficulty != null) {
-        ref = _database.child('users/$userId/results/levels/$levelId/$difficulty/$id');
-      } else {
-        throw Exception('Некорректный тип результата');
+        _measurementResults.removeWhere((result) => result['id'] == id);
+      } else if (type == 'levels') {
+        _levelResults.removeWhere((result) => result['id'] == id);
       }
 
-      await ref.remove();
-      _results.removeWhere((result) => result['id'] == id);
       notifyListeners();
     } catch (e) {
       print('Ошибка удаления результата по ID: $e');
@@ -163,16 +113,19 @@ class ReactionProvider with ChangeNotifier {
   }
 
   Future<void> clearResults() async {
-    if (currentUser == null) {
-      throw Exception('Пользователь не авторизован');
-    }
-
     try {
-      await _database.child('users/$userId/results').remove();
-      _results.clear();
+      await _repository.clearResults();
+      _measurementResults.clear();
+      _levelResults.clear();
       notifyListeners();
     } catch (e) {
       print('Ошибка очистки результатов: $e');
     }
+  }
+
+  // Метод для получения выбранного количества повторений
+  Future<int> getSelectedRepetitions() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('selectedRepetitions') ?? 5; // 5 - значение по умолчанию
   }
 }
--- lib/screens/home_history_screen.dart
+++ lib/screens/home_history_screen.dart
@@ -2,112 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/reaction_provider.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
+
+   @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+
+@override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<ReactionProvider>(context, listen: false).loadResults( type: 'measurements');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final reactionProvider = Provider.of<ReactionProvider>(context);
+    final results = reactionProvider.measurementResults;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'История результатов',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(40), // Высота нижней части
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DropdownButton<SortOrder>(
-                  value: reactionProvider.sortOrder,
-                  onChanged: (value) {
-                    reactionProvider.setSortOrder(value!);
-                  },
-                  items: [
-                    DropdownMenuItem(
-                      value: SortOrder.descending,
-                      child: Text('Новые сверху'),
-                    ),
-                    DropdownMenuItem(
-                      value: SortOrder.ascending,
-                      child: Text('Старые сверху'),
-                    ),
-                  ],
-                ),
-                IconButton(
-                  icon: const Icon(Icons.delete_forever),
-                  onPressed: () {
-                    _showClearHistoryDialog(context, reactionProvider);
-                  },
-                ),
-              ],
-            ),
+        title: const Text('История результатов'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete_forever),
+            onPressed: () {
+              _showClearHistoryDialog(context, reactionProvider);
+            },
           ),
-        ),
+        ],
       ),
-      body: reactionProvider.results.isEmpty
+      body: results.isEmpty
           ? const Center(child: Text('Нет сохраненных результатов'))
-          : Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: reactionProvider.results.length,
-                    itemBuilder: (context, index) {
-                      final result = reactionProvider.results[index];
-                      return Dismissible(
-                        key: Key(result['id']), // Используем уникальный ID из Firebase
-                        direction: DismissDirection.endToStart,
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          padding: const EdgeInsets.only(right: 20),
-                          child: const Icon(Icons.delete, color: Colors.white),
-                        ),
-                        onDismissed: (_) {
-                          // Удаляем элемент по его уникальному ID
-                          reactionProvider.deleteResultById(
-                            type: 'measurements',
-                            id: result['id'], // Передаем уникальный ID
-                           
-                          );
+          : ListView.builder(
+              itemCount: results.length,
+              itemBuilder: (context, index) {
+                final result = results[index];
+                return Dismissible(
+                  key: Key(result['id']), // Уникальный ключ для каждого элемента
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  onDismissed: (_) {
+                    // Удаляем элемент по его уникальному ID
+                    reactionProvider.deleteResultById(
+                      id: result['id'], // Передаем уникальный ID
+                      type: 'measurements', // Указываем тип результата
+                    
+                    );
 
-                          // Показываем SnackBar с возможностью отмены
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: const Text('Запись удалена'),
-                              action: SnackBarAction(
-                                label: 'Отмена',
-                                onPressed: () {
-                                  // Восстанавливаем удаленный элемент
-                                  reactionProvider.addResult(
-                                    type: 'measurements', // Указываем тип результата
-                                    time: result['time'],
-                                  );
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                        child: ListTile(
-                          title: Text('${result['time'].toStringAsFixed(0)} мс'),
-                          subtitle: Text(result['date']),
+                    // Показываем SnackBar с возможностью отмены
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Запись удалена'),
+                        action: SnackBarAction(
+                          label: 'Отмена',
+                          onPressed: () async {
+                            // Восстанавливаем удаленный элемент
+                            await reactionProvider.addResult(
+                              type: 'measurements', // Указываем тип результата
+                              time: result['time'].toDouble(),
+                              date: result['date'], // Передаем оригинальную дату
+                            );
+                          },
                         ),
-                      );
-                    },
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text('${result['time'].toStringAsFixed(0)} мс, Ошибки: ${result['errors']} из ${result['repetitions']}'),
+                    subtitle: Text(result['date']),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Свайпните влево, чтобы удалить запись',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ),
-              ],
+                );
+              },
             ),
     );
   }
